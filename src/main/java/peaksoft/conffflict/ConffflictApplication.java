3fbeac7b49f93dac9214a02a036aef6aa0b0cc98
baisal02 <altynbekuulubajsal@gmail.com>
2025-03-13 03:28:45 +0600

    Initial commit

---
+++ src/main/java/peaksoft/conffflict/ConffflictApplication.java
@@ -0,0 +1,13 @@
+package peaksoft.conffflict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConffflictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConffflictApplication.class, args);
+    }
+
+}
